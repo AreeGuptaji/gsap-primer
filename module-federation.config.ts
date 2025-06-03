9a2903d397089bab2b61875ff217abbd56e15515
Aman Gupta <00amangpt00@gmail.com>
2025-06-03 22:47:11 +0530

    Initial Setup with assets

---
+++ module-federation.config.ts
@@ -0,0 +1,5 @@
+export const mfConfig = {
+  name: "gsap_primer",
+  exposes: {},
+  shared: ["react", "react-dom"],
+};
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+
+const App = () => <div className="mt-10 text-3xl mx-auto max-w-6xl"></div>;
+
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+
+root.render(<App />);
