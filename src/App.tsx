5b2f3db1baaa848d3b822cb57c282d9cf8ff79d6
Aman Gupta <00amangpt00@gmail.com>
2025-06-03 23:03:05 +0530

    Base Layout: Made some changes to folder struture

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const App = () => <div className="mt-10 text-3xl mx-auto max-w-6xl"></div>;
+const App = () => (
+  <div className="mt-10 text-3xl mx-auto max-w-6xl">
+    <h1 className="text-5xl font-robert-medium">Welcome to Awwwards.com</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
 
